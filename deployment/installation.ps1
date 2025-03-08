c3c6ec9a9d51acb53f2b807f4e1839d974f95dce
Alex Remo <aremo@centrilogic.com>
2025-03-08 13:42:13 -0500

    removed branch 8.0.0 from clone command

--- deployment/installation.ps1
+++ deployment/installation.ps1
@@ -3,7 +3,7 @@ chmod +x dotnet-install.sh; `
 ./dotnet-install.sh -version 8.0.303; `
 $ENV:PATH="$HOME/.dotnet:$ENV:PATH"; `
 dotnet tool install --global dotnet-ef --version 8.0.0; `
-git clone https://github.com/AlexBeyd/Commercial-Marketplace-SaaS-Accelerator.git -b 8.0.0 --depth 1; `
+git clone https://github.com/AlexBeyd/Commercial-Marketplace-SaaS-Accelerator.git --depth 1; `
 cd ./Commercial-Marketplace-SaaS-Accelerator/deployment; `
 .\Deploy.ps1 `
  -WebAppNamePrefix "saasottest01" `
