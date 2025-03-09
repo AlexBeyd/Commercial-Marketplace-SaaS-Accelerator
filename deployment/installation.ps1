3d6e64bca66b52e1a1bcb6c49d87ebf1f6bb68eb
Alex Remo <aremo@centrilogic.com>
2025-03-09 11:48:35 -0400

    changed to tag v 8.0.0

--- deployment/installation.ps1
+++ deployment/installation.ps1
@@ -3,10 +3,10 @@ chmod +x dotnet-install.sh; `
 ./dotnet-install.sh -version 8.0.303; `
 $ENV:PATH="$HOME/.dotnet:$ENV:PATH"; `
 dotnet tool install --global dotnet-ef --version 8.0.0; `
-git clone https://github.com/AlexBeyd/Commercial-Marketplace-SaaS-Accelerator.git --depth 1; `
+git clone https://github.com/Azure/Commercial-Marketplace-SaaS-Accelerator.git -b 8.0.0 --depth 1; `
 cd ./Commercial-Marketplace-SaaS-Accelerator/deployment; `
 .\Deploy.ps1 `
- -WebAppNamePrefix "saasottest01" `
- -ResourceGroupForDeployment "saas-accelerator-studies" `
+ -WebAppNamePrefix "aremo-saasottest01" `
+ -ResourceGroupForDeployment "saas-accelerator-studies-v8" `
  -PublisherAdminUsers "beyderman.alex@gmail.com" `
  -Location "Canada Central" 
